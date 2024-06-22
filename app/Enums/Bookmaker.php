5ca8ad97b0cc5f8e31ad8ec8acdaeb4a8bfbf8de
Anton Stepurko <stepurko.anton@gmail.com>
2024-06-22 14:00:57 +0300

    :sparkles: implement Bookmaker enum

---
+++ app/Enums/Bookmaker.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class Bookmaker extends Enum
+{
+    const int ONE = 0;
+    const int TWO = 1;
+    const int THREE = 2;
+}
