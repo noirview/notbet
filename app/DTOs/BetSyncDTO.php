19c604b403f29a452dbbaa3da2517e50fb0b1616
Anton Stepurko <stepurko.anton@gmail.com>
2024-07-28 12:38:30 +0300

    :adhesive_bandage: change type

--- app/DTOs/BetSyncDTO.php
+++ app/DTOs/BetSyncDTO.php
@@ -11,7 +11,7 @@ readonly class BetSyncDTO
         public ?int $numberPeriod,
         public ?int $sign,
         public ?float $value,
-        public ?float $coefficient,
+        public float $coefficient,
         public int $bookmaker,
     ) {}
 }
